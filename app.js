57b91b137ca2d0c42f1ea9c2ce51856f643b909f
SayedAli <sayed.ali.majed@gmail.com>
2025-07-23 19:19:37 +0300

    Compeleted till 5

---
+++ app.js
@@ -0,0 +1,60 @@
+/*
+Exercise 1: Define an empty array
+
+1) Create an empty array and assign it to a variable called `foods`.
+
+Exercise 1 has been completed for you:
+*/
+
+const foods = [];  
+
+console.log('Exercise 1 result:', foods);
+console.log('================================')
+/*
+Exercise 2: Add strings to the array
+
+1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+
+Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
+
+Complete Exercise 2 in the space below:
+*/
+
+
+foods.push('pizza','cheeseburger');
+
+console.log('Exercise 2 result:', foods);
+console.log('=====================================')
+foods.unshift('taco');
+console.log('Exercise 3 result:', foods);
+console.log('=====================================')
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+let favFood
+favFood = foods[1];
+
+console.log('Exercise 4 result:', favFood);
+console.log('================================')
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+
+
+console.log('Exercise 5 result:', foods);
+foods.splice(2,0, 'tofu');
+
+console.log('Exercise 5 result:', foods);
+console.log('================================')
